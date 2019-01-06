10e880754df9f2f79eca17297d7b3f1802dc651a
DemoHn <demohn2016@gmail.com>
2019-01-06 15:05:20 +0800

    feat: use standalone build.sh

--- docker/mce_api/build.sh
+++ docker/mce_api/build.sh
@@ -1,2 +1,12 @@
 #!/bin/bash
-make build
+
+ROOT=/go/src/mingchuan.me
+BIN=/app/bin
+# remove old build
+rm -f $BIN/mce
+rm -rf $ROOT/api/restapi $ROOT/api/models
+
+# genearte swagger files
+swagger generate server -t api -f $ROOT/api/swagger.yml --exclude-main -A mce	
+# build binary
+CGO_ENABLED=0 go build -ldflags "-s -w" -o $BIN/mce -v $ROOT/main.go
