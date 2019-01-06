ff600efbc6569961c3263ad34632ec2acaa002ec
DemoHn <demohn2016@gmail.com>
2019-01-06 15:05:20 +0800

    feat: use standalone build.sh

--- docker/mce_api/start.sh
+++ docker/mce_api/start.sh
@@ -5,10 +5,7 @@ echo "MCE_ENV = $MCE_ENV"
 if [ "$MCE_ENV" = "development" ]
 then
   bash /app/build.sh
-elif [ "$MCE_ENV" = "build" ]
-then
-  bash /app/build.sh
-else
-  # start
-  /app/bin/mce -c /app/config.yml
 fi
+
+# start
+/app/bin/mce -c /app/config.yml
