df10174785f500445cf018622f27005545ed809e
DemoHn <demohn2016@gmail.com>
2019-01-06 14:48:26 +0800

    feat: fix mce_site build

--- docker/mce_site/start.sh
+++ docker/mce_site/start.sh
@@ -2,28 +2,12 @@
 echo "[start] mce_site"
 echo "MCE_ENV = $MCE_ENV"
 
-# set NODE_ENV
-if [ "$MCE_ENV" = "build" ]
-then
-  NODE_ENV=production
-else
-  NODE_ENV=$MCE_ENV
-fi
-echo "NODE_ENV = $NODE_ENV"
-
-# install deps
-yarn
-
 # run build script
 if [ "$MCE_ENV" = "development" ]
 then
-  NODE_ENV=$NODE_ENV bash /app/build.sh
-  yarn dev
-elif [ "$MCE_ENV" = "build" ]
-then
-  NODE_ENV=$NODE_ENV bash /app/build.sh
+  NODE_ENV=$MCE_ENV yarn && yarn dev
 else
   # run production build
   cp -r /app/dist/.nuxt /app/src
-  yarn start
+  NODE_ENV=$MCE_ENV yarn start
 fi
