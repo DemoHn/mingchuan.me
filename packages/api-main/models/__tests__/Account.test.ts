d2606db6a229a2545399dc8fde06ccfb2c9915e5
DemoHn <demohn2016@gmail.com>
2019-06-28 00:04:06 +0800

    feat: add first jest testcase

--- packages/api-main/models/__tests__/Account.test.ts
+++ packages/api-main/models/__tests__/Account.test.ts
@@ -3,6 +3,13 @@ import models from '../index'
 
 describe('Model: Account', () => {
   const dbmigrate = getInstance(true)
+  const { Account } = models
+
+  const payload = {
+    name: '这是一个支持汉字的账户',
+    passwordHash: Buffer.from([1, 2, 3]),
+    permissionMask: 0,
+  }
   beforeAll(async () => {
     await dbmigrate.up()
   })
@@ -12,12 +19,17 @@ describe('Model: Account', () => {
   })
 
   test('should create Account()', async () => {
-    const acct = await models.Account.create({
-      name: 'A',
-      passwordHash: Buffer.from([1, 2, 3]),
-      permissionMask: 0,
+    const acct = await Account.create(payload)
+    expect(acct).toMatchObject(payload)
+  })
+
+  test('should find one Account()', async () => {
+    const result = await Account.findOne({
+      where: {
+        permissionMask: 0,
+      },
     })
 
-    expect(acct).toHaveProperty('name', 'A')
+    expect(result).toMatchObject(payload)
   })
 })
