f1600dc8e8a02d5ef3774c19e09c2562353381e8
DemoHn <demohn2016@gmail.com>
2019-07-07 20:00:38 +0800

    feat: continued login page

--- packages/web-main/pages/admin/login.tsx
+++ packages/web-main/pages/admin/login.tsx
@@ -12,21 +12,41 @@ const MainContainer = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
 `
 
 const FormContainer = styled.div`
   width: 100%;
-  max-width: 350px;
+  max-width: 450px;
+  padding-left: 25px;
+  padding-right: 25px;
+`
+
+const Wrapper = styled.div`
+  box-shadow: 0px 0px 3px 2px rgba(51, 51, 51, 0.15);
+  border-radius: 4px;
+  padding: 30px 20px;
+`
+
+const Banner = styled.div`
+  font-size: 22px;
+  text-align: center;
+  font-family: OCR-A;
+  margin-bottom: 25px;
+  color: #294d05;
 `
 const LoginPage: NextFunctionComponent = () => {
   return (
     <MainContainer>
+      <Banner>Admin Login</Banner>
       <FormContainer>
-        <LoginForm
-          onLogin={data => {
-            console.log(data)
-          }}
-        />
+        <Wrapper>
+          <LoginForm
+            onLogin={data => {
+              console.log(data)
+            }}
+          />
+        </Wrapper>
       </FormContainer>
     </MainContainer>
   )
