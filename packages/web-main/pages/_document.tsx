4f0f923c342e65debdc7503c677e97a84f8b8a0b
DemoHn <demohn2016@gmail.com>
2019-07-07 11:26:52 +0800

    feat: add fonts and global style

---
+++ packages/web-main/pages/_document.tsx
@@ -0,0 +1,40 @@
+import Document, { Html, Head, Main, NextScript, NextDocumentContext } from 'next/document'
+
+const globalStyle = `
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+@font-face{
+  font-family: OCR-A;
+  src: url(/static/OCR-A.ttf);
+  font-weight: normal;
+  font-style: normal;
+}
+`
+class MyDocument extends Document {
+  static async getInitialProps(ctx: NextDocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name='viewport' content='width=device-width, initial-scale=1' />
+          <meta charSet='utf-8' />
+          <style>{globalStyle}</style>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument
--- packages/web-main/pages/index.tsx
+++ packages/web-main/pages/index.tsx
@@ -1,7 +1,25 @@
 import React from 'react'
+import { NextFunctionComponent } from 'next'
+import styled from 'styled-components'
 
-const Index: React.FC = () => {
-  return <div>233</div>
+const MainContainer = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+const Box = styled.div`
+  font-family: OCR-A;
+  font-size: 20px;
+`
+const Index: NextFunctionComponent = () => {
+  return <MainContainer>
+    <Box>
+      mingchuan.me
+    </Box>
+  </MainContainer>
 }
 
 export default Index
