9c77edc881fbe7d6cb727cdfcda7522d1c5c2666
DemoHn <demohn2016@gmail.com>
2019-07-09 21:45:25 +0800

    feat: fix FOCU problem

--- packages/web-main/pages/_document.tsx
+++ packages/web-main/pages/_document.tsx
@@ -5,6 +5,7 @@ import Document, {
   NextScript,
   NextDocumentContext,
 } from 'next/document'
+import { ServerStyleSheet } from 'styled-components'
 
 const globalStyle = `
 * {
@@ -23,8 +24,28 @@ const globalStyle = `
 `
 class MyDocument extends Document {
   static async getInitialProps(ctx: NextDocumentContext) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
+    const sheet = new ServerStyleSheet()
+    const originalRenderPage = ctx.renderPage
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        })
+
+      const initialProps = await Document.getInitialProps(ctx)
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      }
+    } finally {
+      sheet.seal()
+    }
   }
 
   render() {
